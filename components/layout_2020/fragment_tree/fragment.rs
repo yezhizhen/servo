43ed6972a282d82513dd8ba3a481f1715262c3ae
stevennovaryo <steven.novaryo@gmail.com>
2025-03-05 20:19:14 +0800

    Fix clippy
    Signed-off-by: stevennovaryo <steven.novaryo@gmail.com>

--- components/layout_2020/fragment_tree/fragment.rs
+++ components/layout_2020/fragment_tree/fragment.rs
@@ -231,10 +231,7 @@ impl Fragment {
                 let fragment = fragment.borrow();
 
                 let scroll_id = fragment.base.tag.map(|tag| {
-                    ExternalScrollId(
-                        tag.to_display_list_fragment_id(),
-                        pipeline_id.into(),
-                    )
+                    ExternalScrollId(tag.to_display_list_fragment_id(), pipeline_id.into())
                 });
                 let scroll_offset = scroll_id
                     .and_then(|id| scroll_offsets.get(&id))
@@ -279,10 +276,7 @@ impl Fragment {
                 let fragment = fragment.borrow();
 
                 let scroll_id = fragment.base.tag.map(|tag| {
-                    ExternalScrollId(
-                        tag.to_display_list_fragment_id(),
-                        pipeline_id.into(),
-                    )
+                    ExternalScrollId(tag.to_display_list_fragment_id(), pipeline_id.into())
                 });
                 let scroll_offset = scroll_id
                     .and_then(|id| scroll_offsets.get(&id))
